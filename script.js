08502803dc6e06701bb2e267d3f9c0b7f35e18cc
Christer <Christer.Fagerstrand@tetrapak.com>
2025-04-24 18:13:15 +0200

    testat 3 delar

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.querySelector("h1").addEventListener("click", () => {
-    alert("Du klickade på rubriken!");
+    alert("Du klickade pÃ¥ rubriken!");
 });
+
+document.querySelector('#welcome').addEventListener('click', () => {
+    document.querySelector('#welcome').style.backgroundColor = '#e0ffe0';
+});
+
+document.getElementById('welcome').style.backgroundColor = '#e0ffe0';
